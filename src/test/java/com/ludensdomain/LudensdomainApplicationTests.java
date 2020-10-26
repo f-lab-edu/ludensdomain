42d3f599fc12691accd9a62023ff8b9a173d7322
daakludens <heejahn713@gmail.com>
2020-10-27 00:52:50 +0900

    [#1]feat: Create project

---
+++ src/test/java/com/ludensdomain/LudensdomainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ludensdomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LudensdomainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
