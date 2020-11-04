63cce222170b4ec0b8f5f3b70c5231e32bc796b8
daakludens <heejahn713@gmail.com>
2020-11-05 01:07:56 +0900

    [#1] add : Validator 설정 추가

--- src/main/java/com/ludensdomain/dto/UserDto.java
+++ src/main/java/com/ludensdomain/dto/UserDto.java
@@ -13,15 +13,24 @@ public class UserDto {
 
     @NotEmpty(message = "아이디를 입력하세요.")
     Long id;
+
     @NotEmpty(message = "이름을 입력하세요.")
     String name;
+
     @NotEmpty(message = "비밀번호를 입력하세요.")
     @Pattern(regexp = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})", message = "비밀 번호는 6~20자리로 숫자와 특수 문자가 포함된 영문 대소문자로 입력해 주세요")
     String password;
+
+    @NotEmpty
     @Email(message = "이메일 형식으로 입력해주세요.")
     String email;
+
+    @NotEmpty
     Date dateOfBirth;
+
     @Length(max = 11, message = "전화번호는 11자리 이하로 입력해주세요.")
     String phoneNo;
+
+    @NotEmpty
     String role;
 }
