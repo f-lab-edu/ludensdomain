c4078dd55bcbaef3a97616e2301624f72303b42b
daakludens <heejahn713@gmail.com>
2020-11-06 22:00:06 +0900

    [#1] feat: issue 분리를 위해 jasypt 관련 코드 제거

--- src/main/java/com/ludensdomain/LudensdomainApplication.java
+++ src/main/java/com/ludensdomain/LudensdomainApplication.java
@@ -1,12 +1,8 @@
 package com.ludensdomain;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
-import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableEncryptableProperties
 @SpringBootApplication
 public class LudensdomainApplication {
 
