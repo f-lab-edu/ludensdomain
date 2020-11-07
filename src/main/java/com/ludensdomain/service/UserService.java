72655a1cd4bfe276c382e163e19cc5f323e90dfc
daakludens <heejahn713@gmail.com>
2020-11-08 01:06:13 +0900

    [#1] refactor: UserService에 생성자 주입 코드를 @RequiredArgsConstructor로 변경

--- src/main/java/com/ludensdomain/service/UserService.java
+++ src/main/java/com/ludensdomain/service/UserService.java
@@ -2,18 +2,16 @@ package com.ludensdomain.service;
 
 import com.ludensdomain.dto.UserDto;
 import com.ludensdomain.mapper.UserMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
     private final UserMapper userMapper;
-
-    public UserService(UserMapper userMapper) {
-        this.userMapper = userMapper;
-    }
 
     public Optional<UserDto> getUserInfo(long id, String password) {
         return userMapper.getUserInfo();
