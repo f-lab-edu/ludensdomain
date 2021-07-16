1bbcaa63704946db29fc21178013481d15a30fd7
daakludens <heejahn713@gmail.com>
2021-07-16 13:28:49 +0900

    [#25] refactor : RedisConfig 에러의 @Value 값이 제대로 들어가는지 test

--- src/main/java/com/ludensdomain/config/RedisConfig.java
+++ src/main/java/com/ludensdomain/config/RedisConfig.java
@@ -15,7 +15,6 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.session.data.redis.config.ConfigureRedisAction;
 
 import java.time.Duration;
 import java.util.HashMap;
